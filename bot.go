73b36a95cbbe3909dcff6d69cc51c6bd52c8a8c8
叶嘉 <jayec@foxmail.com>
2024-06-11 10:27:19 +0800

    up

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package botclient
 import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
+	"io"
 )
 
 const (
@@ -86,6 +87,35 @@ func (bot *WxWorkBot) UploadMedia(filepath string) (mediaId string, err error) {
 
 	request := bot.httpClient.NewRequest()
 	request.SetFile("media", filepath)
+	request.SetResult(&result)
+	response, err := request.Post(fmt.Sprintf(uploadMediaUrl, bot.key))
+	if err != nil {
+		return "", err
+	}
+
+	if !response.IsSuccess() {
+		return "", fmt.Errorf("upload media failed, status code: %s", response.Status())
+	}
+
+	if result.ErrCode != 0 {
+		return "", fmt.Errorf("upload media failed, err code: %d, err msg: %s", result.ErrCode, result.ErrMsg)
+	}
+
+	return result.MediaId, nil
+}
+
+func (bot *WxWorkBot) UploadMediaReader(fileName string, read io.Reader) (mediaId string, err error) {
+
+	var result struct {
+		Type      string `json:"type"`
+		MediaId   string `json:"media_id"`
+		CreatedAt string `json:"created_at"`
+		ErrCode   int    `json:"errcode"`
+		ErrMsg    string `json:"errmsg"`
+	}
+
+	request := bot.httpClient.NewRequest()
+	request.SetFileReader("media", fileName, read)
 	request.SetResult(&result)
 	response, err := request.Post(fmt.Sprintf(uploadMediaUrl, bot.key))
 	if err != nil {
